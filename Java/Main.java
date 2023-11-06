3eae0d6970ff7abd256ab14e9c037c29122aea38
yamamoto hitoki <brumaire1013@gmail.com>
2023-11-06 19:02:50 +0900

    even number

---
+++ Java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 1問目: 1から10までの偶数を昇順で表示
+        System.out.println("1問目:1から10までの偶数を昇順で表示");
+        for (int i = 2; i <= 10; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
